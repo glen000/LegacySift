41fa0e9a6ee266c973ac89ef3b103fa46e50d1fb
glen000 <183278015+glen000@users.noreply.github.com>
2026-09-10 21:26:18 +0200

    Bump version to 0.2.1-alpha

--- src/LegacySift/Properties/AssemblyInfo.cs
+++ src/LegacySift/Properties/AssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2026")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8e408cbe-5e0a-4e08-b747-4522b3d9d346")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.2.1.0")]
+[assembly: AssemblyFileVersion("0.2.1.0")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("LegacySift.Tests")]
