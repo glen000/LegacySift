f85da10a879b6d593942c8378f18261559ac3614
glen000 <183278015+glen000@users.noreply.github.com>
2026-09-10 20:44:50 +0200

    Add assembly metadata

---
+++ src/LegacySift/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LegacySift")]
+[assembly: AssemblyDescription("Safe old-folder comparison and recovery for computer migrations")]
+[assembly: AssemblyCompany("LegacySift contributors")]
+[assembly: AssemblyProduct("LegacySift")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8e408cbe-5e0a-4e08-b747-4522b3d9d346")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("LegacySift.Tests")]
